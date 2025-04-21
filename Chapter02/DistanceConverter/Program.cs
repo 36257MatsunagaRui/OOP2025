3edc82920bffc59e8de43d65ed7312aaa08285e2
infosys <infosys@C4213>
2025-04-21 09:58:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+           //フィートからメートルへの対応表を出力
+           for (int feet = 1; feet <= 10; feet++) {
+                //double meter = feet * 0.3048;
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+            }
+
+            //メートルからフィートへの対応表を出力
+            for (int meter = 1; meter <= 10; meter++) {
+                //double meter = feet * 0.3048;
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+            }
+        }
+
+        //フィートからメートルへの変換
+        static double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+
+        //メートルからフィートへの変換
+        static double MeterToFeet(int meter) {
+            return meter / 0.3048;
+        }
+    }
+}
