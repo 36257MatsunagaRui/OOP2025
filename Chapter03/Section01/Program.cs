41404eb3a76d65131e7754e5ad78c131a63f3282
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-05-16 14:14:27 +0900

    P68まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -6,7 +6,7 @@
             //var num = int.Parse(Console.ReadLine());
 
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            Console.WriteLine(Count(numbers, delegate(int n) { return n % 2 == 0; }));
+            Console.WriteLine(Count(numbers, n => 5 <= n && n < 10));
         }
 
         static int Count(int[] numbers, Func<int, bool> judge) {
