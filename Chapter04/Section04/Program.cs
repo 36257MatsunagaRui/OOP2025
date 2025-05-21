f9dadf5d6eb28a2e109a83c250c4ec883d24f9a0
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-05-21 12:08:22 +0900

    第4章演習問題開始

---
+++ Chapter04/Section04/Program.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Section04 {
+    internal class Program {
+        static void Main(string[] args) {
+            #region nullの判定
+            string? name = null;
+
+            if (name is null)
+                Console.WriteLine("nameはnullです");
+            #endregion
+
+            #region null合体演算子
+            string code = "12345";
+            var message = GetMessage(code) ?? DefaultMessage();
+            Console.WriteLine(message);
+            #endregion
+
+            #region null条件演算子
+            /*Sale? sale = new Sale {
+                 ShopName = "新宿店",
+                 ProductCategory = "洋菓子",
+                 Amount = 523100
+             };*/
+
+            Sale? sale = null;
+            int? amount = sale?.Amount;
+            Console.WriteLine(sale);
+            #endregion
+
+            #region 値の入れ替え
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("a =" + a + "b =" + b);
+            (b, a) = (a, b);
+            /*swapのアルゴリズム(古いやり方)
+            int temp = a;
+            a = b;
+            b = temp;*/
+            Console.WriteLine("a =" + a + "b =" + b);
+            #endregion
+
+            #region 例外エラー
+            string? inputData = Console.ReadLine();
+
+            if (int.TryParse(inputData, out var number)) {
+                Console.WriteLine(number);
+            } else {
+                Console.WriteLine("エラー");
+            }
+
+            /*try {
+                int num = int.Parse(inputData);
+                Console.WriteLine(num);
+            }
+
+            catch (FormatException e) {
+               　Console.WriteLine("フォーマットエラー");
+                //Console.WriteLine(e.Message);
+            }
+
+            catch (OverflowException) {
+                Console.WriteLine("入力値が大きすぎます");
+            
+            }
+
+            finally {
+                Console.WriteLine("処理完了");
+            }
+
+            Console.WriteLine("メソッド終了");*/
+            #endregion
+        }
+
+        private static object? GetMessage(string code) {
+            return null;
+        }
+
+        private static object? DefaultMessage() {
+            return "DefaultMessage";
+        }
+
+        //売り上げクラス
+        public class Sale {
+            //店舗名
+            public string ShopName { get; set; } = string.Empty;
+            //商品カテゴリ
+            public string ProductCategory { get; set; } = string.Empty;
+            //売上高
+            public int Amount { get; set; }
+        }
+    }
+}
