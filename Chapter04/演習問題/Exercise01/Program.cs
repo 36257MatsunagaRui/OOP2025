28b9a9f8d3dddea471b774c269557b2aeba9e975
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-05-23 14:45:04 +0900

    演習問題4.1.3完成

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -15,15 +15,36 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-            
+            foreach (var lang in langs) {
+                if (lang.Contains('S'))
+                    Console.WriteLine(lang);
+            }
+            Console.WriteLine("");
+
+            for (var i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S'))
+                    Console.WriteLine(langs[i]);
+            }
+            Console.WriteLine("");
+
+            int index = 0;
+            while (index < langs.Count) {
+                if (langs[index].Contains('S'))
+                    Console.WriteLine(langs[index]);
+                index++;
+            }
         }
 
         private static void Exercise2(List<string> langs) {
-            
+            var selected = langs.Where(s => s.Contains('S'));
+            foreach (var lang in selected) {
+                Console.WriteLine(lang);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
-            
+            var lang = langs.Find(s => s.Length == 10) ?? ("unknown");
+            Console.WriteLine(lang);
         }
     }
 }
