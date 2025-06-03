66ed1867a052848f9b91bae0f534ef64fc423bc7
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-06-03 12:10:37 +0900

    演習問題6.3.3

---
+++ Chapter06/Section03/Section03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Section03 {
+    internal class Program {
+        static void Main(string[] args) {
+            /*var languages = new[] { "C#", "Java", "Python", "Ruby", };
+            var separator = ",";
+            var result = String.Join(separator, languages);
+            Console.WriteLine(result);*/
+
+            //StringBuilderを利用
+            var sb = new StringBuilder();
+            foreach (var word in GetWords()) {
+                sb.Append(word);
+            }
+            var text = sb.ToString();
+            Console.WriteLine(text);
+
+            //文字列の連結
+            string str = "";
+            foreach (var word in GetWords()) {
+                str += word;
+            }
+            Console.WriteLine(str);
+        }
+
+        private static IEnumerable<object> GetWords() {
+            return ["Orange", "Apple", "Grape"];
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            foreach (var pair in line.Split(';')) {
+                var word = pair.Split('=');
+                Console.WriteLine($"{ToJapanese(word[0])}:{word[1]}");
+            }
+
+            /*for (int i = 0; i < array.Length; i++) {
+                    var word = array[i].Split('=');
+                    Console.WriteLine($"{ToJapanese(word[0])}:{word[1]}");
+            }*/
+        }
+
+            /// <summary>
+            /// 引数の単語を日本語へ変換します
+            /// </summary>
+            /// <param name="key">"Novelist","BestWork","Born"</param>
+            /// <returns>"「作家」,「代表作」,「誕生年」</returns>
+        static string ToJapanese(string key) {
+            return key switch {
+                "Novelist" => "作家",
+                "BestWork" => "代表作",
+                "Born" => "誕生年",
+                _ => "引数keyは、正しい値ではありません"
+            };
+        }
+    }
+}
