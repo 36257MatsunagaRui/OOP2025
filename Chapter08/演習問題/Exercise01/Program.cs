678b94adbb2144f1d94c6a9365f6c4f1e289ba43
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-06-11 11:11:09 +0900

    演習問題8.1.1

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            //ディクショナリインスタンスの生成
+            var dict = new Dictionary<char, int>();
+
+            //1文字取り出す→大文字に変換
+            foreach (var ch in text.ToUpper().OrderBy(c => c)) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch)) {
+                        dict[ch]++;
+                    }else {
+                        dict[ch] = 1;
+                    }
+                }     
+            }
+
+            foreach (var (key, value) in dict) {
+                Console.WriteLine($"'{key}':{value}");
+            }
+        }
+
+        private static void Exercise2(string text) {
+            //ディクショナリインスタンスの生成
+            var dict = new SortedDictionary<char, int>();
+
+            //1文字取り出す→大文字に変換
+            foreach (var ch in text.ToUpper()) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch)) {
+                        dict[ch]++;
+                    } else {
+                        dict[ch] = 1;
+                    }
+                }
+            }
+
+            foreach (var (TKey, TValue) in dict) {
+                Console.WriteLine($"'{TKey}':{TValue}");
+            }
+        }
+    }
+}
