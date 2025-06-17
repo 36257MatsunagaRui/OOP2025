af0f5e6de62a23db334e28fa42b1b2723db3d4b7
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-06-17 11:15:49 +0900

    設問1_Studentクラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public string Name { get; init; } = string.Empty; //学生の名前
+        public string subject { get; init; } = string.Empty; //科目名
+        public int Score { get; init; }  //点数
+    }
+}
