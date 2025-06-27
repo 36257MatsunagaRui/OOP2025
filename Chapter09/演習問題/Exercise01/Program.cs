47beabfdedca6761e13ae6e3e415cd456c58c31c
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-06-27 13:47:29 +0900

    演習問題9.3

---
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            //2024/03/09 19:03
+            //string Formatを使った例
+            var date1 = string.Format($"{dateTime:yyyy/MM/dd HH:mm}");
+            Console.WriteLine(date1);
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            //2024年03月09日 19時03分09秒
+            //DateTime ToStringを使った例
+            var date2 = dateTime.ToString($"{dateTime:yyyy年MM月dd日 HH時mm分ss秒}");
+            Console.WriteLine(date2);
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var culture = new CultureInfo("jp-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            /*和暦2桁表示(ゼロサプレスなし)
+            var str = dateTime.ToString("ggyy年MM月dd日", culture);
+            var dayOfWeek = culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+            Console.WriteLine(str + dateTime.ToString("(ddd曜日)", culture));*/
+
+            //和暦2桁表示(ゼロサプレスなし)
+            var datestr = dateTime.ToString("ggy", culture);
+            var dayOfWeek = culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+            var str = string.Format($"{datestr}年{dateTime.Month,2}月{dateTime.Day,2}日({dayOfWeek})");
+            Console.WriteLine(str);
+
+            //和暦２桁表示（ゼロサプレスあり）
+            var cul = dateTime.ToString("gg", culture);
+            var year = int.Parse(dateTime.ToString("yy", culture));
+            var str2 = string.Format($"{cul}{year,2}年{dateTime.Month,2}月{dateTime.Day,2}日({dayOfWeek})");
+            Console.WriteLine(str2);
+        }
+    }
+}
---
+++ Chapter09/演習問題/Exercise02/Program.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_1();  //テストケース１
+            Console.WriteLine();
+            Exercise1_2();//テストケース２
+            Console.WriteLine();
+            Exercise1_3();//テストケース３
+            Console.WriteLine();
+            Exercise2();
+        }
+        // 9.2.1を呼び出すテスト用メソッド
+        private static void Exercise1_1() {
+            var dt = new DateTime(2024, 7, 1);
+            foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek))) {
+                Console.Write("{0:yyyy/MM/dd}の次週の{1}: ", dt, (DayOfWeek)dayofweek);
+                Console.WriteLine("{0:yyyy/MM/dd(ddd)}", NextWeek(dt, (DayOfWeek)dayofweek));
+            }
+        }
+
+        // 9.2.1を呼び出すテスト用メソッド
+        private static void Exercise1_2() {
+            var dt = new DateTime(2024, 8, 29);
+            foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek))) {
+                Console.Write("{0:yyyy/MM/dd}の次週の{1}: ", dt, (DayOfWeek)dayofweek);
+                Console.WriteLine("{0:yyyy/MM/dd(ddd)}", NextWeek(dt, (DayOfWeek)dayofweek));
+            }
+        }
+
+        // 9.2.1を呼び出すテスト用メソッド
+        private static void Exercise1_3() {
+            var dt = DateTime.Now;
+            foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek))) {
+                Console.Write("{0:yyyy/MM/dd}の次週の{1}: ", dt, (DayOfWeek)dayofweek);
+                Console.WriteLine("{0:yyyy/MM/dd(ddd)}", NextWeek(dt, (DayOfWeek)dayofweek));
+            }
+        }
+
+        // 9.2.1【ここにプログラムを作成する】
+        static DateTime NextWeek(DateTime date, DayOfWeek dayOfWeek) {
+            //一週間後の日付を求める(AddDays(7))
+            /*var nextweek = date.AddDays(7);
+            //一週間後の日曜の日付を求める 日曜(0) - 金曜(5) = -5
+            var days = (int)dayOfWeek - (int)(date.DayOfWeek);
+            //一週間後の日付から5日戻す (AddDays(-5))
+            return nextweek.AddDays(days);*/
+
+            var days = 7 - (int)(date.DayOfWeek) + (int)dayOfWeek;
+            return date.AddDays(days);
+
+            /*var days = (int)dayOfWeek - (int)(date.DayOfWeek);
+            return date.AddDays(days + 7);*/
+        }
+
+        private static void Exercise2() {
+            var birthday = new DateOnly(2001, 4, 19);
+            var targetDay = new DateOnly(2030, 4, 18);
+            var age = GetAge(birthday, targetDay);
+            Console.WriteLine(age);
+        }
+
+        // 9.2.2【ここにプログラムを作成する】
+        static int GetAge(DateOnly birthday, DateOnly targetDay) {
+            var age = targetDay.Year - birthday.Year;
+            if (targetDay < birthday.AddYears(age)) {
+                age--;
+            }
+            return age;
+        }
+    }
+}
