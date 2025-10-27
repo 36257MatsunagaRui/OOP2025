18444d134a9114f98120e1df52a8983b2e21cdfa
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-10-27 15:28:55 +0900

    Exercise1_2

---
+++ Chapter13/Section01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var price = Library.Books
+                .Where(b => b.CategoryId == 1)
+                .Max(b => b.Price);
+            Console.WriteLine(price);
+
+            Console.WriteLine();
+
+            var book = Library.Books
+                .Where(x => x.PublishedYear >= 2021)
+                .MinBy(x => x.Price);
+            Console.WriteLine(book);
+
+            Console.WriteLine();
+
+            var average = Library.Books.Average(x => x.Price);
+            var aboves = Library.Books.Where(b => b.Price > average);
+            foreach (var book1 in aboves) {
+                Console.WriteLine(book1);
+            }
+        }
+    }
+}
