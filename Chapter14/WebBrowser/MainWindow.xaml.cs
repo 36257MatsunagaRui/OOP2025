1b4f665fb6b46645953ef3133eeb1829e0dd4ac9
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-11-06 13:46:35 +0900

    途中経過

---
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebBrowser;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BackButton_Click(object sender, RoutedEventArgs e) {
+        Webview.GoBack();
+    }
+
+    private void FowardButton_Click(object sender, RoutedEventArgs e) {
+        Webview.GoForward();
+    }
+
+    private void GoButton_Click(object sender, RoutedEventArgs e) {
+        var url = AddressBar.Text;
+        Webview.Source = new Uri(url);
+    }
+}
