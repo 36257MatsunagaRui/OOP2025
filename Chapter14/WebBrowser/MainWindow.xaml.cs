17f073ea34039eca174e3b860bf99c87116cc979
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-11-06 14:41:50 +0900

    プログレスバー実装

--- Chapter14/WebBrowser/MainWindow.xaml.cs
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Web.WebView2.Core;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,18 +20,43 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        InitializeAsync();
+    }
+
+    private async void InitializeAsync() {
+        await Webview.EnsureCoreWebView2Async();
+        Webview.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+        Webview.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+    }
+
+    //読み込み開始したらプログレスバーを表示
+    private void CoreWebView2_NavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs e) {
+        LoadingBar.Visibility = Visibility.Visible;
+        LoadingBar.IsIndeterminate = true;
+    }
+
+    //読み込み完了したらプログレスバーを非表示
+    private void CoreWebView2_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e) {
+        LoadingBar.Visibility = Visibility.Collapsed;
+        LoadingBar.IsIndeterminate = false;
     }
 
     private void BackButton_Click(object sender, RoutedEventArgs e) {
-        Webview.GoBack();
+        if (Webview.CanGoBack) {
+            Webview.GoBack();
+        }
     }
 
     private void FowardButton_Click(object sender, RoutedEventArgs e) {
-        Webview.GoForward();
+        if (Webview.CanGoForward) {
+            Webview.GoForward();
+        }
     }
 
     private void GoButton_Click(object sender, RoutedEventArgs e) {
         var url = AddressBar.Text;
+
+        if (string.IsNullOrWhiteSpace(url)) return;
         Webview.Source = new Uri(url);
     }
 }
