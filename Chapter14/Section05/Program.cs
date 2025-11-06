f8d6783e34d88172b0b60cf7af4b0d2aa241612b
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-11-06 15:17:51 +0900

    並列処理

---
+++ Chapter14/Section05/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("並列処理あり");
+
+            Parallel.For(0, 100, i => {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(500);
+                Console.WriteLine($"処理{i}終了");
+            });
+
+            Console.WriteLine("");
+            Console.WriteLine("並列処理なし");
+
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(1000);
+                Console.WriteLine($"処理{i}終了");
+            }
+        }
+    }
+}
