8868e2ab4c15be31a9d7ebb544f6b5a56f154901
36257MatsunagaRui <oibc36257@ojs.ac.jp>
2025-11-19 09:33:09 +0900

    問題15.3完成

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    //問題15.1
+    internal class LineToHalfNumberService : ITextFileService {
+        public void Execute(string line) {
+
+        }
+
+        public void Initialize(string fname) {
+
+        }
+
+        public void Terminate() {
+
+        }
+    }
+}
--- Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -1,7 +1,9 @@
 ﻿namespace TextFileProcessorDI {
     internal class Program {
         static void Main(string[] args) {
-            var service = new LineCounterService();
+            //var service = new LineCounterService();
+            var service = new LineOutputService();
+            //var service = new LineToHalfNumberService();
             var processor = new TextFileProcessor(service);
             Console.Write("パスの入力:");
             processor.Run(Console.ReadLine());
